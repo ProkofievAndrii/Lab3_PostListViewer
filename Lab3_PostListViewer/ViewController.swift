4c14720a458c7ef1eacce0af7a114b1fbac95011
Prokofiev Andrii <prokofiev.main@gmail.com>
2024-02-24 18:52:26 +0200

    Initial Commit

---
+++ Lab3_PostListViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab3_PostListViewer
+//
+//  Created by Andrii Prokofiev on 24.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
